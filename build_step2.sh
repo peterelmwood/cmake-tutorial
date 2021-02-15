c89a0993d88092a22e1e0797f5379d95d5de14e2
Peter E. Wood <peter.wood@cybirical.com>
2021-02-15 10:23:28 -0600

    end step2

--- build_step2.sh
+++ build_step2.sh
@@ -1,9 +1,10 @@
 RED='\033[0;31m'
+GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 echo "setup"
 cd /cmake/
-[ ! -d "/cmake/step1_build" ] && mkdir step2_build
+[ ! -d "/cmake/step2_build" ] && mkdir step2_build
 cd step2_build
 
 # set -USE_MYMATH back to default
@@ -12,15 +13,15 @@ cmake -U USE_MYMATH .
 echo "${RED}**** create executable with custom sqrt ****${NC}"
 cmake ../Step2 .
 cmake --build .
-OUTPUT=$({./Tutorial)
-echo -e "${RED}${OUTPUT}${NC}"
+OUTPUT=$(./Tutorial)
+echo -e "${GREEN}${OUTPUT}${NC}"
 OUTPUT=$(./Tutorial 100)
 echo -e "${RED}${OUTPUT}${NC}"
 
 echo "${RED}**** create executable without custom sqrt ****${NC}"
 cmake ../Step2 -DUSE_MYMATH=OFF .
 cmake --build .
-OUTPUT=$({./Tutorial)
-echo -e "${RED}${OUTPUT}${NC}"
+OUTPUT=$(./Tutorial)
+echo -e "${GREEN}${OUTPUT}${NC}"
 OUTPUT=$(./Tutorial 100)
 echo -e "${RED}${OUTPUT}${NC}"
