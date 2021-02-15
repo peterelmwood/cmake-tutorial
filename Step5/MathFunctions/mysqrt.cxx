df5cbb69a806090cd3b6de6a80ae913c363e81ff
Peter E. Wood <peter.wood@cybirical.com>
2021-02-15 12:26:48 -0600

    Step5 complete

--- Step5/MathFunctions/mysqrt.cxx
+++ Step5/MathFunctions/mysqrt.cxx
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 #include "MathFunctions.h"
 
@@ -9,7 +10,13 @@ double mysqrt(double x)
     return 0;
   }
 
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+  double result = exp(log(x) * 0.5);
+  std::cout << "Computing sqrt of " << x << " to be " << result
+            << " using log and exp" << std::endl;
+#else
   double result = x;
+#endif
 
   // do ten iterations
   for (int i = 0; i < 10; ++i) {
